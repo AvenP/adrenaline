63af193f33a2422e71bd69307711414f6d97d6ca
PhilippWinston <philippwinston@gmail.com>
2023-07-29 21:14:25 -0500

    added the files for models and filled them out, added into the auth

--- src/server/models/Category.js
+++ src/server/models/Category.js
@@ -3,6 +3,5 @@ const { Schema } = mongoose;
 const categorySchema = new Schema({
   categoryName: { type: String, required: true },
 });
-
 const Category = mongoose.model("Category", categorySchema);
 module.exports = Category;
