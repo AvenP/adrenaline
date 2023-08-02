1443c47fa25f1ecc6b64fea85e2c6d29d35a1592
robobrownie13 <86945332+robobrownie13@users.noreply.github.com>
2023-08-01 21:12:14 -0500

    added queries for workout

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const Workout = require("./Workout");
 const userSchema = new Schema({
-  userName: { type: String, required: true, trim: true },
+  username: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true, unique: true },
   password: { type: String, required: true, trim: true, minLength: 8 },
   workouts: [Workout.schema],
