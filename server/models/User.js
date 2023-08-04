b5f4e85a5ee0d9efea97e3f43634fbf770c28d36
PhilippWinston <philippwinston@gmail.com>
2023-08-03 22:03:48 -0500

    fixed the issue for the login, added bcrypt to the login.js, user.js, and the resolovers

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const Workout = require("./Workout");
+const bcrypt = require("bcrypt"); // Import bcrypt library
+
 const userSchema = new Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true, unique: true },
   password: { type: String, required: true, trim: true, minLength: 8 },
   workouts: [Workout.schema],
 });
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
